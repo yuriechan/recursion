4000962b7f702603d0a40f45db05c23cb5a553ce
Yurie <cutecupcakeandcute@gmail.com>
2020-02-17 17:36:34 +0900

    keep sample solution unchanged

--- patternCounter.js
+++ patternCounter.js
@@ -3,10 +3,7 @@ const patternCounter = (string, pattern, count = 0) => {
         return count;
     if (string.slice(0, pattern.length) === pattern) {
         count++;
-        console.log(string)
-        console.log(string.slice(0, pattern.length))
-        console.log(string.slice(pattern.length))
-        return patternCounter(string.slice(pattern.length), pattern, count);
+        return patternCounter(string.slice(pattern.length - 1), pattern, count);
     } else {
         return patternCounter(string.slice(1), pattern, count);
     }
